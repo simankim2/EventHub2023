a584532fa30379776435d928e0d02d9acf36726e
Siman <simankim2@gmail.com>
2023-12-01 02:31:19 -0500

    CRUD functionality added

--- src/features/events/EventsList.tsx
+++ src/features/events/EventsList.tsx
@@ -3,13 +3,19 @@ import EventListItem from "./dashboard/EventListItem";
 
 type Props = {
   events: AppEvent[]
+  selectEvent: (event: AppEvent) => void
+  deleteEvent: (eventId: string) => void
 }
 
-export default function EventList({events}: Props) {
+export default function EventList({events, selectEvent, deleteEvent}: Props) {
   return (
     <>
       {events.map((event: any) => (
-        <EventListItem key={event.id} event={event} />
+        <EventListItem
+          key={event.id} event={event}
+          selectEvent={selectEvent}
+          deleteEvent={deleteEvent}
+        />
       ))}
     </>
   )
