bc46f0dc96285a88e6781243006d3063144eec8a
Siman <simankim2@gmail.com>
2024-02-19 01:46:40 -0500

    removing test component

--- src/app/common/modals/ModalManager.tsx
+++ src/app/common/modals/ModalManager.tsx
@@ -1,11 +1,9 @@
 import LoginForm from "../../../features/auth/LoginForm";
 import RegisterForm from "../../../features/auth/RegisterForm";
-import TestModal from "../../../features/scratch/TestModal";
 import { useAppSelector } from "../../store/store"
 
 export default function ModalManager() {
   const modalLookup = {
-    TestModal,
     LoginForm,
     RegisterForm
   }
--- src/app/layout/nav/NavBar.tsx
+++ src/app/layout/nav/NavBar.tsx
@@ -16,7 +16,6 @@ export default function NavBar() {
                 EventHub
             </MenuItem>
             <MenuItem name='Events' as={NavLink} to='/events' />
-            <MenuItem name='Scratch' as={NavLink} to='/scratch' />
             <MenuItem>
                 <Button
                     as={NavLink}
