d7f4b9b47ae795089adf78a7d6f3dbd53ccdd4b0
Siman <simankim2@gmail.com>
2024-02-19 01:30:04 -0500

    removed test component no longer needed

--- src/app/router/Routes.tsx
+++ src/app/router/Routes.tsx
@@ -3,7 +3,6 @@ import App from "../layout/App";
 import EventDashboard from "../../features/events/dashboard/EventDashboard";
 import EventDetailedPage from "../../features/events/details/EventDetailedPage";
 import EventForm from "../../features/events/form/EventForm";
-import Scratch from "../../features/scratch/Scratch";
 import AccountPage from "../../features/auth/AccountPage";
 import ProfilePage from "../../features/profiles/ProfilePage";
 import RequireAuth from "./RequireAuth";
@@ -22,7 +21,6 @@ export const router = createBrowserRouter([
       ]},
       {path: '/events', element: <EventDashboard />},
       {path: '/events/:id', element: <EventDetailedPage />},
-      {path: '/scratch', element: <Scratch />},
       {path: '/unauthorized', element: <UnauthComponent />},
     ]
   }
